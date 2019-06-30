18b10644e3550e37d308718a5a495f1d6c3f9f60
Dante Cantal <danteacantal@gmail.com>
2019-06-30 11:41:25 -0400

    first commit

---
+++ src/Palette.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Palette extends Component {
+    render() {
+        return (
+            <div className="Palette">
+                {/* Navbar goes here */}
+                <div className="Palette-colors">
+                    {/* bunch of colors */}
+                </div>
+                {/* footer goes here */}
+                <h1>PALETTE</h1>
+            </div>
+        )
+    }
+}
+
+export default Palette;
